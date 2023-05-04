5642ad3433e5db05fd468bce7c65723814396e71
unknown <sonirawat4968@gmail.com>
2023-05-04 22:51:03 +0530

    firstcommit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+    public function store(Request $request ){
+     //upload image
+     $imageName = time().'.'.$request->image->extension();
+     $request->image->move(public_path('products'),$imageName);
+    
+      
+     $product = new Product;
+     $product->image = $imageName;
+     $product->name = $request->name;
+     $product->description = $request->description;
+     $product->save();
+     return back();
+   
+    }
+}
